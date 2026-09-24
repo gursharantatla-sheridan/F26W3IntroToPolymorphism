c5f2cfe6dbd32dca640142c3f004b0612e31d19a
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-24 13:33:30 -0400

    compile time polymorphism

--- F26W3IntroToPolymorphism/Program.cs
+++ F26W3IntroToPolymorphism/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Add(2, 3);
+
+            int[] a = { 1, 2, 3 };
+            PrintArray(a);
+            //PrintArray(2, 3, 4, 5);
         }
+
+
+        static void PrintArray(int[] arr)
+        {
+
+        }
+
+        //static void PrintArray(params int[] arr)
+        //{
+
+        //}
+
+
+
+
+        static void Add(int n1, int n2)
+        {
+
+        }
+
+        //static int Add(int n1, int n2)
+        //{
+        //    return n1 + n2;
+        //}
+
+        static void Add(string s1, string s2)
+        {
+
+        }
+
+        static void Add(int n1, int n2, int n3)
+        {
+
+        }
+
+        static void Add(int n1, int n2, out int n3)
+        {
+            n3 = 5;
+        }
+
+        
     }
 }
