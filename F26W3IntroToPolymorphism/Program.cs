55ab57943e8cd2fbe7d4ffe631887ba37ffe3a25
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-24 13:19:15 -0400

    Add project files.

---
+++ F26W3IntroToPolymorphism/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace F26W3IntroToPolymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
